dc5e2b2e6d96003e280a5d89b754f8f77b00705e
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-21 23:58:30 +0530

    aaaaaaaaa

--- EF6CodeFirstDemo/Grade.cs
+++ EF6CodeFirstDemo/Grade.cs
@@ -7,7 +7,6 @@ namespace EF6CodeFirstDemo
         public int GradeId { get; set; }
         public string GradeName { get; set; }
         public string Section { get; set; }
-
         public virtual ICollection<Student> Students { get; set; }
     }
 }
